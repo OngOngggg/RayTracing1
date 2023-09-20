59cee5a5d016d00a155a8f747b652d6c2a123c9f
yxzzz <1095091088@qq.com>
2023-09-20 16:08:28 +0800

    dielectric and refraction

--- RayTracing1/material.h
+++ RayTracing1/material.h
@@ -28,15 +28,40 @@ public:
 //金属材料
 class metal :public material {
 public:
-	metal(const color& a) :albedo(a) { }
+	metal(const color& a, double f) :albedo(a), fuzz(f < 1 ? f : 1) { }
 
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
 		vec3 reflected = reflect(unit_vector(r_in.getDirection()), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
 		attenuation = albedo;
 		return (dot(scattered.getDirection(), rec.normal) > 0);
 	}
 public:
 	color albedo;
+	//模糊度，因为金属表面太大，反射可以不按正常反射来发生（或许是金属球不是绝对光滑的原因？加这个模糊度用来改变正确的反射光线走向
+	//模糊度最大取1
+	double fuzz;
+};
+
+//玻璃材料
+class dielectric :public material {
+public:
+	dielectric(double r):ref_idx(r){}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+		
+		attenuation = color(1.0, 1.0, 1.0);
+		
+		double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx;
+		
+		vec3 unit_direction = unit_vector(r_in.getDirection());
+		vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+		scattered = ray(rec.p, refracted);
+		return true;
+	}
+
+public:
+	//折射率，由内到外的时候是1/ref_idx
+	double ref_idx;
 };
 
--- RayTracing1/vec3.h
+++ RayTracing1/vec3.h
@@ -147,3 +147,14 @@ inline vec3 unit_vector(const vec3& v) {
 vec3 reflect(const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n) * n;
 }
+
+//返回折射光线，n是切线方向向量，etai_over_etat折射率
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+	//cos值
+	auto cos_theta = dot(-uv, n);
+	//折射后的光线垂直分量
+	vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+	//折射后的光线水平分量
+	vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+	return r_out_perp + r_out_parallel;
+}
