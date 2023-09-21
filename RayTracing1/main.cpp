c4dd2fbe7c545076773dd016e7c21d8cac62ce77
yxzzz <1095091088@qq.com>
2023-09-21 10:40:30 +0800

    more reflect!

--- RayTracing1/main.cpp
+++ RayTracing1/main.cpp
@@ -84,13 +84,14 @@ int main()
 	hittable_list world;
 	//都是书中的例子
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<dielectric>(2);
+	auto material_center = make_shared<dielectric>(1.7);
 	auto material_left = make_shared<dielectric>(1.5);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2),0.7);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 	//world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
 	//world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
--- RayTracing1/material.h
+++ RayTracing1/material.h
@@ -5,6 +5,13 @@
 
 struct hit_record;
 
+//更加近似的算法，用来算折射率
+double schlick(double cosine, double ref_idx) {
+	auto r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 *= r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
 class material {
 public:
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
@@ -55,13 +62,29 @@ public:
 		double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx;
 		
 		vec3 unit_direction = unit_vector(r_in.getDirection());
+
+		double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+		//考虑反射的情况，sin和n相乘大于1不能发生折射
+		if (etai_over_etat * sin_theta > 1.0)
+		{
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
+		double reflect_prob = schlick(cos_theta, etai_over_etat);//反射率
+		if (random_double() < reflect_prob) {
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
 		vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
 		scattered = ray(rec.p, refracted);
 		return true;
 	}
 
 public:
-	//折射率，由内到外的时候是1/ref_idx
+	//折射率，由内到外的时候是1/ref_idx,玻璃一般是1.3-1.7
 	double ref_idx;
 };
 
