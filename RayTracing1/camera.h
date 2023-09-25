0a22f625a3a68bb6096a053041013ec99c7577e7
yxzzz <1095091088@qq.com>
2023-09-25 15:06:51 +0800

    forgot to commit, almost RayTracing in one month...4h for a single picture...

--- RayTracing1/camera.h
+++ RayTracing1/camera.h
@@ -18,6 +18,23 @@ public:
 		vertical = vec3(0, viewport_height, 0.0);
 		lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 	}
+
+	camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio)
+	{
+		auto theta = degrees_to_radians(vfov);
+		auto h = tan(theta / 2);
+		auto viewport_height = 2.0 * h;
+		auto viewport_width = aspect_ratio * viewport_height;
+
+		auto w = unit_vector(lookfrom - lookat);
+		auto u = unit_vector(cross(vup, w));
+		auto v = cross(w, u);
+
+		origin = lookfrom;
+		horizontal = viewport_width * u;
+		vertical = viewport_height * v;
+		lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+	}
 	
 	ray get_ray(double u, double v)
 	{
--- RayTracing1/main.cpp
+++ RayTracing1/main.cpp
@@ -13,6 +13,54 @@ using std::cout;
 
 const int max_depth = 50;
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));//地面
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double(); //随机材质
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double()); //随机中心
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse 漫反射
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal 金属
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass 玻璃
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
+}
+
 //判断光线是否与某个球相交
 //double hit_sphere(const point3& center, double radius, const ray& r) {
 //	vec3 oc = r.getOrigin() - center;
@@ -75,15 +123,26 @@ int main()
 		}
 	}*/
 
-	const double aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const double aspect_ratio = 4.0 / 3.0;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	//抗锯齿模拟随机次数
 	const int sample_per_pixel = 100;
 
-	hittable_list world;
+	//World
+	auto world = random_scene();
+
+	//Camera
+	point3 lookfrom(12, 2, 3);
+	point3 lookat(0, 0, 0);
+	vec3 vup(0, 1, 0);
+	//auto dist_to_focus = 10.0;
+	//auto aperture = 0.1;
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio);
+
 	//都是书中的例子
-	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+
+	/*auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	auto material_center = make_shared<dielectric>(1.7);
 	auto material_left = make_shared<dielectric>(1.5);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2),0.7);
@@ -92,11 +151,9 @@ int main()
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
-	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-	//world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	//world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));*/
 
-	camera cam;
+	//camera cam;
 	
 	cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 	for (int j = image_height - 1; j >= 0; --j) {
